222f32569e6de46a4d2e23463e4d2e098cc3b61b
omar <doctoromar253@gmail.com>
2025-01-16 17:06:47 +0000

    trying dry

---
+++ src/components/footer.js
@@ -0,0 +1,58 @@
+export const footer = `
+      <footer class="SiteFooter_core-footer__VWM8c">
+      <div
+        class="SiteFooter_wrapper__qFXh_ wrapper xs:grid xs:grid-2 s:grid-12"
+      >
+        <div class="SiteFooter_logo-mark__pkgIW xs:col-start-1 xs:col-span-1">
+          <a aria-label="AI logo mark" href="#">
+            <h1 style="font-weight: bold">Gatara AI</h1></a
+          >
+          <h1><a aria-label="AI logo mark" href="#"> </a></h1>
+        </div>
+        <div
+          class="SiteFooter_footer-top__rZBHh xs:col-start-1 xs:col-span-2 s:grid s:grid-6 s:col-start-5 s:col-span-8 m:col-start-5 m:col-span-6"
+        >
+          <ul class="s:col-start-1 s:col-span-2">
+            <li>
+              <a href="https://murshed.gatara.org">Murshed </a>
+            </li>
+
+            <li>
+              <a href="#">Company </a>
+            </li>
+
+            <li>
+              <a href="#">Careers </a>
+            </li>
+          </ul>
+          <hr class="SiteFooter_hrule__6yi4j" />
+          <ul class="s:col-start-3 s:col-span-2">
+            <li>
+              <a href="mailto:info@gatara.org">Press Inquiries </a>
+            </li>
+
+            <li>
+              <a href="#" rel="noopener" target="_blank">LinkedIn </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/gatara.ai"
+                rel="noopener"
+                target="_blank"
+                >Instagram
+              </a>
+            </li>
+            <li>
+              <a href="#" rel="noopener" target="_blank">Facebook </a>
+            </li>
+          </ul>
+
+          <div
+            class="SiteFooter_footer-bottom__1251A xs:col-start-1 xs:col-span-2 s:col-start-5 s:col-span-8 m:col-start-11 m:col-span-2"
+          >
+            <div class="SiteFooter_copyright__YACmd">© 2025 Gatara AI</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+`;
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// Function to inject components
+function injectComponents() {
+    // Inject header
+    const headerPlaceholder = document.querySelector('#header-placeholder');
+    if (headerPlaceholder) {
+      import('../components/header.js')
+        .then(module => {
+          headerPlaceholder.innerHTML = module.header;
+        });
+    }
+  
+    // Inject footer
+    const footerPlaceholder = document.querySelector('#footer-placeholder');
+    if (footerPlaceholder) {
+      import('../components/footer.js')
+        .then(module => {
+          footerPlaceholder.innerHTML = module.footer;
+        });
+    }
+  }
+  
+  // Call the function when DOM is loaded
+  document.addEventListener('DOMContentLoaded', injectComponents);
